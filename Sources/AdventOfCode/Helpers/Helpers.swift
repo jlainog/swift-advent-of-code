c7bc8640c04d7afb9371f69f295fd8ad2d244f84
Jaime Andres Laino Guerra <jlainog@gmail.com>
2022-05-14 20:02:19 -0500

    add ds_store to gitignore

---
+++ Sources/AdventOfCode/Helpers/Helpers.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jaime Andres Laino Guerra on 14/05/22.
+//
+
+import Foundation
---
+++ Tests/AdventOfCodeTests/Day01Tests.swift
@@ -0,0 +1,47 @@
+import XCTest
+import swift_advent_of_code
+
+class Day1Tests: XCTestCase {
+    let day = Day1()
+
+    func testPart1() throws {
+        XCTAssertEqual(
+            day.part1(
+                """
+                199
+                200
+                208
+                210
+                200
+                207
+                240
+                269
+                260
+                263
+                """
+            ) as? Int,
+            7
+        )
+    }
+
+    func testPart2() throws {
+        XCTAssertEqual(
+            day.part2(
+                """
+                199
+                200
+                208
+                210
+                200
+                207
+                240
+                269
+                260
+                263
+                """
+            ) as? Int,
+            5
+        )
+    }
+
+}
